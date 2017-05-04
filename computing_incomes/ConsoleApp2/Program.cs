832c09f95b7f707d2dc7a3a3e47fca423bc40e8e
User <User>
2017-05-04 17:36:40 +0400

    cs commit

---
+++ computing_incomes/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gunluk_gelir = Convert.ToInt16(Console.ReadLine());
+            int gun_sayi = Convert.ToInt16(Console.ReadLine());
+            int brut_gelir = gunluk_gelir * gun_sayi;
+            double faiz;
+
+            if(brut_gelir >= 1000)
+            {
+                faiz = brut_gelir * 0.18;
+            }
+            else
+            {
+                faiz = brut_gelir * 0.04;
+            }
+
+            double net_gelir = brut_gelir - faiz;
+
+            Console.WriteLine("Faiz: " + faiz);
+            Console.WriteLine("Net gelir: " + net_gelir);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ even_odd/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int reqem = Convert.ToInt32(Console.ReadLine());
+
+                if (reqem % 2 == 0)
+                {
+                    reqem = (int)Math.Pow(reqem, 2);
+                }
+                else
+                {
+                    reqem = (int)Math.Pow(reqem, 3);
+                }
+                Console.WriteLine(reqem);
+                Console.ReadKey();
+        }
+    }
+}
